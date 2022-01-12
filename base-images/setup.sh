74e605627ff294c0205178927ef95ef7f4bccf8b
zdm <zdm@softvisio.net>
2022-01-12 13:52:52 +0200

    fix: base images postgresql added

--- base-images/setup.sh
+++ base-images/setup.sh
@@ -3,10 +3,14 @@
 set -u
 set -e
 
+POSTGRESQL_VERSION=14
+
 apt update
 
 apt install -y curl
 
 source <(curl -fsSL https://raw.githubusercontent.com/softvisio/scripts/main/setup-host.sh)
 
-apt install -y apt-utils git gcc g++ make cmake libssl-dev gpg
+apt install -y \
+    apt-utils git gcc g++ make cmake libssl-dev gpg \
+    postgresql-server-dev-$POSTGRESQL_VERSION libkrb5-dev libipc-run-perl
