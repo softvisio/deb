f4060213b9f3a4bb2683e918243f409671b54f81
zdm <zdm@softvisio.net>
2024-03-24 12:34:27 +0200

    feat: ported to js

--- bin/build.js
+++ bin/build.js
@@ -1,1 +1,19 @@
 #!/usr/bin/env node
+
+import Cli from "#core/cli";
+
+const CLI = {
+    "title": `Debian packages repository manager`,
+
+    "commands": {
+        "build": {
+            "title": `build deb package`,
+        },
+
+        "update": {
+            "title": `Update deb repository`,
+        },
+    },
+};
+
+await Cli.parse( CLI );
