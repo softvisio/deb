92487c8579dae56e9f05f044a47af4655cdb6838
zdm <zdm@softvisio.net>
2024-04-27 21:39:41 +0300

    fix: repo repamed

--- install.sh
+++ install.sh
@@ -12,10 +12,10 @@ rm -rf /etc/apt/sources.list.d/${REPO_NAME}.list
 
 apt-get clean all
 
-curl -fsSLo /usr/share/keyrings/${REPO_NAME}-archive-keyring.gpg https://raw.githubusercontent.com/${REPO_SLUG}/dists/keyring.gpg
+curl -fsSLo /usr/share/keyrings/${REPO_NAME}-archive-keyring.gpg https://raw.githubusercontent.com/$REPO_SLUG/dists/keyring.gpg
 
 cat << EOF > /etc/apt/sources.list.d/${REPO_NAME}.list
-deb [signed-by=/usr/share/keyrings/${REPO_NAME}-archive-keyring.gpg] https://raw.githubusercontent.com/${REPO_SLUG}/ $(. /etc/os-release && echo $VERSION_ID) $COMPONENT
+deb [signed-by=/usr/share/keyrings/${REPO_NAME}-archive-keyring.gpg] https://raw.githubusercontent.com/$REPO_SLUG/ $(. /etc/os-release && echo $VERSION_ID) $COMPONENT
 
 # deb [trusted=yes] https://raw.githubusercontent.com/$REPO_SLUG/ $(. /etc/os-release && echo $VERSION_ID) $COMPONENT
 EOF
