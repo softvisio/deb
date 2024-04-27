c79a67c8487670c8c08d2c5c88bf2ab3c553c74e
zdm <zdm@softvisio.net>
2024-04-27 21:46:08 +0300

    fix: repo repamed

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ set -e
 REPO_NAME=softvisio
 REPO_SLUG=softvisio/apt/main
 COMPONENT=main
+VERSION_ID=$(. /etc/os-release && echo $VERSION_ID)
 
 rm -rf /etc/apt/sources.list.d/${REPO_NAME}.list
 
@@ -15,7 +16,7 @@ apt-get clean all
 curl -fsSLo /usr/share/keyrings/${REPO_NAME}-archive-keyring.gpg https://raw.githubusercontent.com/$REPO_SLUG/dists/keyring.gpg
 
 cat << EOF > /etc/apt/sources.list.d/${REPO_NAME}.list
-deb [signed-by=/usr/share/keyrings/${REPO_NAME}-archive-keyring.gpg] https://raw.githubusercontent.com/$REPO_SLUG/ $(. /etc/os-release && echo $VERSION_ID) $COMPONENT
+deb [signed-by=/usr/share/keyrings/${REPO_NAME}-archive-keyring.gpg] https://raw.githubusercontent.com/$REPO_SLUG/ $VERSION_ID $COMPONENT
 
-# deb [trusted=yes] https://raw.githubusercontent.com/$REPO_SLUG/ $(. /etc/os-release && echo $VERSION_ID) $COMPONENT
+# deb [trusted=yes] https://raw.githubusercontent.com/$REPO_SLUG/ $VERSION_ID $COMPONENT
 EOF
